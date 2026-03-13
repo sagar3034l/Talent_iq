2ca21c348586ad3031b1296c3e8157655d5eef47
sagar3034l <sd8778610@gmail.com>
2026-03-13 17:53:05 +0530

    Fix catch-all route for static assets in production

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,7 +46,7 @@ const __dirname = path.resolve();
     
 if(ENV.NODE_ENV === "production"){
    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-   app.get('/{*any}',(req,res)=>{
+   app.get('*',(req,res)=>{
       res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
    })
 }
